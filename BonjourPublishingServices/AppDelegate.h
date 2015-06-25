af6dd45f5700dec896eaf4b6704a600770a5bee5
Stephane Hildbrand <stephane.hildbrand@savantsystems.com>
2015-06-25 15:23:13 -0700

    Initial Commit

---
+++ BonjourPublishingServices/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BonjourPublishingServices
+//
+//  Created by Stephane Hildbrand on 6/25/15.
+//  Copyright (c) 2015 Stephane Hildbrand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BonjourPublishingServices/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BonjourPublishingServices
+//
+//  Created by Stephane Hildbrand on 6/25/15.
+//  Copyright (c) 2015 Stephane Hildbrand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
